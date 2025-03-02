fa442b4aeb48d7784424b1e19764cb1e09e41a81
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2025-03-02 22:35:00 +0100

    Modify readFile method

--- src/app/FileHandler.java
+++ src/app/FileHandler.java
@@ -1,4 +1,34 @@
 package app;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileHandler {
+
+    private final static String BASE_PATH = "files/";
+
+    public String writeFile(String fileContent) {
+        String fileName;
+        try (FileWriter fw = new FileWriter(new File(fileName + ".txt")) {
+                fw.write(fileContent);
+                return"Success.";
+        } catch(Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    public String readFile(String path) {
+        try (FileReader reader = new FileReader(path)) {
+            int sym;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((sym = reader.read()) != -1) {
+                stringBuilder.append((char) sym);
+            }
+            return stringBuilder.toString();
+        } catch (IOException ex) {
+            return ex.getMessage();
+        }
+    }
 }
